69714d737d37b98229dd8839bd67557a630d0382
Air-KS <kevinrogeret@gmail.com>
2024-02-09 10:57:25 +0100

    Modif comment // by /* */

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -13,31 +13,31 @@ int is_palindrome(listint_t **head)
 	listint_t *stack = NULL, *temp;
 	int isPalin = 1;
 
-	// une liste vide est considérée comme un palindrome
+	/* une liste vide est considérée comme un palindrome */
 	if (!head || !*head || !(*head)->next)
 	{
 		return (1);
 	}
 
-	// Utiliser deux pointeurs pour trouver le milieur de la liste
+	/* Utiliser deux pointeurs pour trouver le milieur de la liste */
 	while (fast && fast->next)
 	{
 		fast = fast->next->next;
 
-		// Empiler la première moitié des éléments
+		/* Empiler la première moitié des éléments */
 		temp = slow;
 		slow = slow->next;
 		temp->next = stack;
 		stack = temp;
 	}
 
-	// Si la liste a un nombre impaire d'éléments, sauter l'éléments du milieu
+	/* Si la liste a un nombre impaire d'éléments, sauter l'éléments du milieu */
 	if (fast)
 	{
 		slow = slow->next;
 	}
 
-	// Compare les élément de la pile avec la seconde moitié de la list
+	/* Compare les élément de la pile avec la seconde moitié de la list */
 	while (slow)
 	{
 		if (stack->n != slow->n)
@@ -50,7 +50,7 @@ int is_palindrome(listint_t **head)
 		stack = stack->next;
 	}
 
-	// Libérer la pile si necessaire
+	/* Libérer la pile si necessaire */
 	while (stack)
 	{
 		temp = stack->next;
