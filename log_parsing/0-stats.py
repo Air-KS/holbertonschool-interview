334f58fbe641e7ee1ce13e0f442c6ef38781e841
Air-KS <kevinrogeret@gmail.com>
2024-02-16 10:50:55 +0100

    Corrig pycodestyle

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-""" """
+""" Script to parse logs and display statistics """
 import sys
 
 stats = {
     '200': 0,
-    '301' : 0,
-    '400' : 0,
-    '401' : 0,
-    '403' : 0,
-    '404' : 0,
-    '400' : 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '400': 0,
 }
 sizes = [0]
 
 
 def print_stats_code():
-    """ """
+    """ Displays the statistics of status codes and the total file size """
     print('File size: {}'.format(sum(sizes)))
     for http_stats_code, count in sorted(stats.items()):
         if count:
             print('{}: {}'.format(http_stats_code, count))
 
+
 try:
-    for i, line in enumerate(sys.stdin, start=1):
+    for index, line in enumerate(sys.stdin, start=1):
         matches = line.rstrip().split()
         try:
             status_code = matches[-2]
@@ -32,7 +33,7 @@ try:
             sizes.append(int(file_size))
         except Exception:
             pass
-        if i % 10 == 0:
+        if index % 10 == 0:
             print_stats_code()
     print_stats_code()
 except KeyboardInterrupt:
