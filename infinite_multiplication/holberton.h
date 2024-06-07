9232525dd3cfe3cd0e8e6cba2c94ed1be26b203f
Air-KS <kevinrogeret@gmail.com>
2024-06-07 10:31:29 +0200

    Infinite Multiplication

--- infinite_multiplication/holberton.h
+++ infinite_multiplication/holberton.h
@@ -10,9 +10,5 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void multiply(char *num_1, char *num_2);
-int is_a_number(char *a_str);
-void sum_and_print(char answer[MAX1][MAX2], int len_1, int len_2);
-int str_len(char *a);
 
 #endif /*_MULTI_H_*/
